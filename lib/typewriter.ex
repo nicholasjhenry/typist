dc334383b44f916eb655fb55e700f5d43645b219
Nicholas Henry <nicholas.henry@gmail.com>
2019-08-24 11:12:30 -0400

    refactor: Starting again, re-implement single case union type

--- lib/typewriter.ex
+++ lib/typewriter.ex
@@ -3,99 +3,42 @@ defmodule TypeWriter do
   A DSL for defining types inspired by F#.
   """
 
+  # Types:
+  # -
+
   defmacro __using__(_opts) do
     quote do
       import TypeWriter
     end
   end
 
-  # deftype/1
-  # Pattern match on `::` in `deftype ProductCode :: String.t()`, aka "inline syntax"
-  defmacro deftype({:"::", _, args}) do
-    type_name = get_type_name(args)
-    type = get_type(args)
+  defmacro deftype(ast) do
+    type = maybe_single_case_union_type(ast)
 
     quote do
-      defmodule __MODULE__.unquote(Module.concat([type_name])) do
-        defstruct value: nil
-        @type t :: %__MODULE__{value: unquote(type).t()}
+      defmodule unquote(Module.concat([type.name])) do
+        def __type__ do
+          unquote(Macro.escape(type))
+        end
       end
     end
   end
 
-  # deftype/1
-  # Pattern match on deftype String.t(), aka "module syntax"
-  defmacro deftype({{:., _, [{:__aliases__, _, [type]}, :t]}, _, _}) do
-    quote do
-      defstruct value: nil
-      @type t :: %__MODULE__{value: unquote(type).t()}
-    end
+  defmodule SingleCaseUnionType do
+    defstruct [:name, :type]
   end
 
-  defmacro deftype(do: {:__block__, _, ast}) do
-    fields = extract_fields(ast)
-
-    struct_body = Enum.map(fields, fn {field, default, _type} -> {field, default} end)
-
-    typespec =
-      Enum.map(fields, fn
-        {field, _default, {module, type}} -> {field, Module.concat([module, type])}
-        {field, _default, type} -> {field, type}
-      end)
-
-    quote do
-      defstruct unquote(struct_body)
-      @type t :: %__MODULE__{unquote_splicing(typespec)}
-    end
-  end
-
-  # The name of the type is contained in the next nested line.
-  # [
-  #   {:__aliases__, [line: 6], [:ProductCode]},
-  #   _type
-  # ]
-  defp get_type_name([{:__aliases__, _, [type]}, _type]) do
-    type
-  end
-
-  # The type spec is contained in the second nested line.
-  # [
-  #   _type_name,
-  #   {{:., [line: 6], [{:__aliases__, [line: 6], [:String]}, :t]}, [line: 6], []}
-  # ]
-  defp get_type([_type_name, {{:., _, [{:__aliases__, _, [type]}, :t]}, _, _}]) do
-    type
-  end
-
-  # deftype/2
-  defmacro deftype({:__aliases__, _, [type_name]}, do: {:__block__, [], ast}) do
-    fields = extract_fields(ast)
-
-    struct_body = Enum.map(fields, fn {field, default, _type} -> {field, default} end)
-
-    typespec =
-      Enum.map(fields, fn
-        {field, _default, {module, type}} -> {field, Module.concat([module, type])}
-        {field, _default, type} -> {field, type}
-      end)
-
-    quote do
-      defmodule __MODULE__.unquote(Module.concat([type_name])) do
-        defstruct unquote(struct_body)
-        @type t :: %__MODULE__{unquote_splicing(typespec)}
-      end
-    end
-  end
-
-  defp extract_fields(ast) do
-    Enum.map(ast, fn
-      #   {:"::", [line: 29], [ {:code, [line: 29], nil}, {{:., [line: 29], [{:__aliases__, [line: 29], [:ProductCode1]}, :t]}, [line: 29], []} ]},
-      {:"::", _, [{field, _, default}, {{:., _, [{:__aliases__, _, [module]}, type]}, _, []}]} ->
-        {field, default, {module, type}}
-
-      # {:"::", [line: 30], [{:price, [line: 30], nil}, {:float, [line: 30], []}]}
-      {:"::", _, [{field, _, default}, {type, _, []}]} ->
-        {field, default, type}
-    end)
+  # deftype ProductCode1 :: String.t()
+  def maybe_single_case_union_type(
+        {:"::", _,
+         [
+           {:__aliases__, _, [module]},
+           {{:., _, [{:__aliases__, _, [type_name]}, type_function]}, _, []}
+         ]}
+      ) do
+    %TypeWriter.SingleCaseUnionType{
+      name: module,
+      type: {type_name, type_function, []}
+    }
   end
 end
--- test/typewriter_test.exs
+++ test/typewriter_test.exs
@@ -6,46 +6,11 @@ defmodule TypeWriterTest do
   describe "defining a single case union type" do
     deftype ProductCode1 :: String.t()
 
-    test "using inline syntax generates the struct" do
-      alias TypeWriterTest.ProductCode1
-
-      assert ProductCode1.__struct__() == %ProductCode1{value: nil}
-    end
-
-    defmodule ProductCode2 do
-      use TypeWriter
-
-      deftype String.t()
-    end
-
-    test "using module syntax generates the struct" do
-      assert ProductCode2.__struct__() == %ProductCode2{value: nil}
-    end
-  end
-
-  deftype Product1 do
-    code :: ProductCode1.t()
-    price :: float()
-  end
-
-  describe "defining a record" do
-    test "using inline syntax generates the struct" do
-      alias TypeWriterTest.Product1
-
-      assert Product1.__struct__() == %Product1{code: nil, price: nil}
-    end
-
-    defmodule Product2 do
-      use TypeWriter
-
-      deftype do
-        code :: ProductCode1.t()
-        price :: float()
-      end
-    end
-
-    test "using module syntax generates the struct" do
-      assert Product2.__struct__() == %Product2{code: nil, price: nil}
+    test "defines the meta data" do
+      assert ProductCode1.__type__() == %TypeWriter.SingleCaseUnionType{
+               name: :ProductCode1,
+               type: {:String, :t, []}
+             }
     end
   end
 end
