4808762404b65a92fef2d453c647efb93961c2bb
Nicholas Henry <nicholas.henry@gmail.com>
2019-08-24 13:45:43 -0400

    refactor: Implement inline product type

--- lib/typewriter.ex
+++ lib/typewriter.ex
@@ -16,6 +16,8 @@ defmodule TypeWriter do
     current_module = current_module(__CALLER__.module)
     type = maybe_single_case_union_type(current_module, ast)
 
+    type = maybe_product_type(current_module, ast, type)
+
     if module_defined?(current_module, type.name) do
       quote do
         def __type__ do
@@ -35,6 +37,9 @@ defmodule TypeWriter do
 
   # https://fsharpforfunandprofit.com/posts/designing-with-types-single-case-dus/
   defmodule SingleCaseUnionType do
+    @moduledoc """
+    Single case union type is used to wrap a primitive.
+    """
     defstruct [:name, :type]
   end
 
@@ -42,15 +47,15 @@ defmodule TypeWriter do
   # deftype ProductCode1 :: String.t()
   def maybe_single_case_union_type(
         _current_module,
-        {:"::", _,
-         [
-           {:__aliases__, _, [module]},
-           {{:., _, [{:__aliases__, _, [type_name]}, type_function]}, _, []}
-         ]}
+        {
+          :"::",
+          _,
+          [{:__aliases__, _, [module]}, {{:., _, [_, _]}, _, []} = type_to_be_wrapped]
+        }
       ) do
     %TypeWriter.SingleCaseUnionType{
       name: module,
-      type: {type_name, type_function, []}
+      type: get_type(type_to_be_wrapped)
     }
   end
 
@@ -62,13 +67,55 @@ defmodule TypeWriter do
   # end
   def maybe_single_case_union_type(
         current_module,
-        {{:., _, [{:__aliases__, _, [type_name]}, type_function]}, _, []}
+        {{:., _, [_, _]}, _, []} = type_to_be_wrapped
       ) do
     %TypeWriter.SingleCaseUnionType{
       name: current_module,
-      type: {type_name, type_function, []}
+      type: get_type(type_to_be_wrapped)
     }
   end
+
+  def maybe_single_case_union_type(_current_module, _ast), do: :none
+
+  defp get_type({{:., _, [{:__aliases__, _, [type_name]}, type_function]}, _, []}) do
+    {type_name, type_function, []}
+  end
+
+  # Product type - inline
+  # deftype FirstLast :: {String.t(), String.t()}
+  defp maybe_product_type(
+         _current_module,
+         {
+           :"::",
+           _,
+           [{:__aliases__, _, [module]}, product_types]
+         },
+         :none
+       ) do
+    type_info = product_types |> Tuple.to_list() |> Enum.map(&get_type/1) |> List.to_tuple()
+
+    %TypeWriter.SingleCaseUnionType{
+      name: module,
+      type: type_info
+    }
+  end
+
+  # Product type - module
+  # defmodule FirstLast2 do
+  #   use TypeWriter
+  #
+  #   deftype {String.t(), String.t()}
+  # end
+  defp maybe_product_type(current_module, product_types, :none) do
+    type_info = product_types |> Tuple.to_list() |> Enum.map(&get_type/1) |> List.to_tuple()
+
+    %TypeWriter.SingleCaseUnionType{
+      name: current_module,
+      type: type_info
+    }
+  end
+
+  defp maybe_product_type(_current_module, _ast, type), do: type
 
   defp current_module(caller_module) do
     Module.split(caller_module) |> Enum.reverse() |> List.first() |> String.to_atom()
--- test/typewriter_test.exs
+++ test/typewriter_test.exs
@@ -26,4 +26,28 @@ defmodule TypeWriterTest do
              }
     end
   end
+
+  describe "product type" do
+    deftype FirstLast1 :: {String.t(), String.t()}
+
+    test "inline defines the meta data" do
+      assert FirstLast1.__type__() == %TypeWriter.SingleCaseUnionType{
+               name: :FirstLast1,
+               type: {{:String, :t, []}, {:String, :t, []}}
+             }
+    end
+
+    defmodule FirstLast2 do
+      use TypeWriter
+
+      deftype {String.t(), String.t()}
+    end
+
+    test "module defines the meta data" do
+      assert FirstLast2.__type__() == %TypeWriter.SingleCaseUnionType{
+               name: :FirstLast2,
+               type: {{:String, :t, []}, {:String, :t, []}}
+             }
+    end
+  end
 end
