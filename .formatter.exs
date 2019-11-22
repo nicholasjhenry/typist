3ea8c58fe13f5b3db21da22330a7cc2ad2ee10d5
Nicholas Henry <nicholas.henry@gmail.com>
2019-11-22 15:20:46 -0500

    chore: Update formatter

--- .formatter.exs
+++ .formatter.exs
@@ -1,5 +1,5 @@
 # Used by "mix format"
 [
   inputs: ["{mix,.formatter}.exs", "{config,lib,test}/**/*.{ex,exs}"],
-  locals_without_parens: [deftype: 1, deftype: 2]
+  locals_without_parens: [deftype: 1, deftype: 2, deftype: 3]
 ]
