54285e6e9cb982e9a12f1d4992b68f4639376fbb
Nicholas Henry <nicholas.henry@gmail.com>
2019-11-27 10:00:58 -0500

    test: Add product type acceptance test for product type

---
+++ test/acceptance/product_type_test.exs
@@ -0,0 +1,28 @@
+defmodule Typist.ProductTypeTest do
+  use ExUnit.Case
+  use Typist
+
+  describe "defining the type inline" do
+    deftype FirstLast :: {String.t(), integer}
+
+    test "defines the type meta-data" do
+      metadata = FirstLast.__type__()
+
+      assert metadata.ast == {{:String, :t}, :integer}
+    end
+  end
+
+  describe "defining the type in a module" do
+    defmodule Foo.FirstLast do
+      use Typist
+
+      deftype {String.t(), integer}
+    end
+
+    test "defines the type meta-data" do
+      metadata = Foo.FirstLast.__type__()
+
+      assert metadata.ast == {{:String, :t}, :integer}
+    end
+  end
+end
