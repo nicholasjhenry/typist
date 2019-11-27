ece7947b538325d85a895046c3f8cf88055c2391
Nicholas Henry <nicholas.henry@gmail.com>
2019-11-27 10:22:11 -0500

    feat: Implement inline and module record type AST

--- lib/typist.ex
+++ lib/typist.ex
@@ -10,6 +10,25 @@ defmodule Typist do
     end
   end
 
+  # Inline record
+  defmacro deftype(ast, do: block) do
+    record(__CALLER__.module, ast, block)
+  end
+
+  def record(calling_module, module_ast, block_ast) do
+    module = Parser.parse(module_ast)
+    fields = Parser.parse(block_ast)
+    metadata = %{ast: fields}
+
+    build(calling_module, module, metadata)
+  end
+
+  # Module record
+  defmacro deftype(do: block) do
+    type(__CALLER__.module, block)
+  end
+
+  # Union and product types (inline or module)
   defmacro deftype(ast) do
     type(__CALLER__.module, ast)
   end
@@ -19,6 +38,20 @@ defmodule Typist do
     metadata = %{ast: ast}
 
     build(calling_module, metadata)
+  end
+
+  defp build(calling_module, {module_name, :t}, metadata) do
+    module = Module.concat([calling_module, module_name])
+
+    quote do
+      alias unquote(module)
+
+      defmodule unquote(module) do
+        def __type__ do
+          unquote(Macro.escape(metadata))
+        end
+      end
+    end
   end
 
   defp build(calling_module, %{ast: {:"::", _, [{module_name, :t}, type]}} = metadata) do
---
+++ test/acceptance/record_type_test.exs
@@ -0,0 +1,32 @@
+defmodule Typist.RecordTypeTest do
+  use ExUnit.Case
+  use Typist
+
+  describe "defining the type inline" do
+    deftype Product do
+      code :: String.t()
+      price :: integer
+    end
+
+    test "defines the type meta-data" do
+      metadata = Product.__type__()
+
+      assert metadata.ast == {{:code, {:String, :t}}, {:price, :integer}}
+    end
+  end
+
+  describe "defining the type in a module" do
+    defmodule Foo.Product do
+      deftype do
+        code :: String.t()
+        price :: integer
+      end
+    end
+
+    test "defines the type meta-data" do
+      metadata = Foo.Product.__type__()
+
+      assert metadata.ast == {{:code, {:String, :t}}, {:price, :integer}}
+    end
+  end
+end
