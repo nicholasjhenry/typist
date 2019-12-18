aceb5d9f3adcb2def72c42a2fbf99a3c5dbed39b
Nicholas Henry <nicholas.henry@gmail.com>
2019-12-18 15:37:34 -0500

    refactor: Create the Type struct outside of AST generation

--- lib/typist.ex
+++ lib/typist.ex
@@ -19,6 +19,115 @@ defmodule Typist do
     type(__CALLER__.module, block)
   end
 
+  # Define a record type inline
+  defp type(caller_module, {:__aliases__, _metadata, module_name}, block) do
+    spec = Code.to_spec(block)
+    struct = Code.to_struct(block)
+    type = %Type{caller_module: caller_module, module_name: module_name, spec: spec}
+
+    content =
+      quote do
+        unquote(struct)
+
+        def __type__ do
+          unquote(Macro.escape(%{type | spec: Macro.to_string(type.spec)}))
+        end
+      end
+
+    Code.module(type, content)
+  end
+
+  # Define a record type in module
+  defp type(_caller_module, do: block) do
+    spec = Code.to_spec(block)
+    struct = Code.to_struct(block)
+    type = %Type{spec: spec}
+
+    quote location: :keep do
+      unquote(struct)
+
+      def __type__ do
+        unquote(Macro.escape(%{type | spec: Macro.to_string(type.spec)}))
+      end
+    end
+  end
+
+  # Define a dscriminated union type inline
+  defp type(caller_module, {:"::", _, [{:__aliases__, _, module_name}, {:|, _, _} = type]}) do
+    spec =
+      quote do
+        @type t :: unquote(type)
+      end
+
+    type = %Type{spec: spec, caller_module: caller_module, module_name: module_name}
+
+    content =
+      quote do
+        defstruct [:value]
+
+        def __type__ do
+          unquote(Macro.escape(%{type | spec: Macro.to_string(type.spec)}))
+        end
+      end
+
+    Code.module(type, content)
+  end
+
+  # Define a dscriminated union type in a module
+  defp type(_caller_module, {:|, _, _} = type) do
+    spec =
+      quote do
+        @type t :: unquote(type)
+      end
+
+    type = %Type{spec: spec}
+
+    quote location: :keep do
+      def __type__ do
+        unquote(Macro.escape(%{type | spec: Macro.to_string(type.spec)}))
+      end
+    end
+  end
+
+  # Define a single case union type inline
+  defp type(caller_module, {:"::", _, [{:__aliases__, _, module_name}, type]}) do
+    spec =
+      quote do
+        @type t :: %__MODULE__{value: unquote(type)}
+      end
+
+    type = %Type{spec: spec, caller_module: caller_module, module_name: module_name}
+
+    content =
+      quote do
+        defstruct [:value]
+
+        def __type__ do
+          unquote(Macro.escape(%{type | spec: Macro.to_string(type.spec)}))
+        end
+      end
+
+    Code.module(type, content)
+  end
+
+  # Define a single case union or product type inside module
+  defp type(_caller_module, type) do
+    spec =
+      quote do
+        @type t :: %__MODULE__{value: unquote(type)}
+      end
+
+    type = %Type{spec: spec}
+
+    quote do
+      defstruct [:value]
+
+      def __type__ do
+        unquote(Macro.escape(%{type | spec: Macro.to_string(type.spec)}))
+      end
+    end
+  end
+
   defmacro defunion(ast, block) do
     union(__CALLER__.module, ast, block)
   end
@@ -66,109 +175,5 @@ defmodule Typist do
 
   defp union_spec([head | tail]) do
     {:|, [], [head, union_spec(tail)]}
-  end
-
-  # Define a record type inline
-  defp type(caller_module, {:__aliases__, _metadata, module_name}, block) do
-    spec = Code.to_spec(block)
-    struct = Code.to_struct(block)
-    type = %Type{caller_module: caller_module, module_name: module_name, spec: spec}
-
-    content =
-      quote do
-        unquote(struct)
-
-        def __type__ do
-          unquote(Macro.escape(%{type | spec: Macro.to_string(type.spec)}))
-        end
-      end
-
-    Code.module(type, content)
-  end
-
-  # Define a record type in module
-  defp type(_caller_module, do: block) do
-    spec = Code.to_spec(block)
-    struct = Code.to_struct(block)
-
-    quote location: :keep do
-      unquote(struct)
-
-      def __type__ do
-        %Type{spec: unquote(Macro.to_string(spec))}
-      end
-    end
-  end
-
-  # Define a dscriminated union type inline
-  defp type(caller_module, {:"::", _, [{:__aliases__, _, module_name}, {:|, _, _} = type]}) do
-    spec =
-      quote do
-        @type t :: unquote(type)
-      end
-
-    type = %Type{spec: spec, caller_module: caller_module, module_name: module_name}
-
-    content =
-      quote do
-        defstruct [:value]
-
-        def __type__ do
-          unquote(Macro.escape(%{type | spec: Macro.to_string(type.spec)}))
-        end
-      end
-
-    Code.module(type, content)
-  end
-
-  # Define a dscriminated union type in a module
-  defp type(_caller_module, {:|, _, _} = type) do
-    spec =
-      quote do
-        @type t :: unquote(type)
-      end
-
-    quote location: :keep do
-      def __type__ do
-        %Type{spec: unquote(Macro.to_string(spec))}
-      end
-    end
-  end
-
-  # Define a single case union type inline
-  defp type(caller_module, {:"::", _, [{:__aliases__, _, module_name}, type]}) do
-    spec =
-      quote do
-        @type t :: %__MODULE__{value: unquote(type)}
-      end
-
-    type = %Type{spec: spec, caller_module: caller_module, module_name: module_name}
-
-    content =
-      quote do
-        defstruct [:value]
-
-        def __type__ do
-          unquote(Macro.escape(%{type | spec: Macro.to_string(type.spec)}))
-        end
-      end
-
-    Code.module(type, content)
-  end
-
-  # Define a single case union or product type inside module
-  defp type(_caller_module, type) do
-    spec =
-      quote do
-        @type t :: %__MODULE__{value: unquote(type)}
-      end
-
-    quote do
-      defstruct [:value]
-
-      def __type__ do
-        %Type{spec: unquote(Macro.to_string(spec))}
-      end
-    end
   end
 end
