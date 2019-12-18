c27047a9356e79ff43c9a3d48d7a12c7cf2a419e
Nicholas Henry <nicholas.henry@gmail.com>
2019-12-18 10:50:02 -0500

    feat: Define a record type in a module

--- lib/typist.ex
+++ lib/typist.ex
@@ -11,6 +11,10 @@ defmodule Typist do
     type(__CALLER__.module, ast, block)
   end
 
+  defmacro deftype(block) do
+    type(__CALLER__.module, block)
+  end
+
   def type(caller_module, ast, block) do
     {:__aliases__, _metadata, module_name} = ast
     alias_name = Module.concat([caller_module] ++ [List.first(module_name)])
@@ -28,6 +32,19 @@ defmodule Typist do
         def __type__ do
           %{spec: unquote(Macro.to_string(spec))}
         end
+      end
+    end
+  end
+
+  def type(_caller_module, do: block) do
+    spec = Code.to_spec(block)
+    struct = Code.to_struct(block)
+
+    quote location: :keep do
+      unquote(struct)
+
+      def __type__ do
+        %{spec: unquote(Macro.to_string(spec))}
       end
     end
   end
--- test/acceptance/record_type_test.exs
+++ test/acceptance/record_type_test.exs
@@ -15,4 +15,20 @@ defmodule Typist.RecordTypeTest do
       assert metadata.spec == "@type(t :: %__MODULE__{code: String.t(), price: integer()})"
     end
   end
+
+  describe "defining the type in a module" do
+    defmodule Foo.Product do
+      deftype do
+        code :: String.t()
+        price :: integer
+      end
+    end
+
+    test "defines the type meta-data" do
+      metadata = Foo.Product.__type__()
+
+      assert %Foo.Product{code: "ABC", price: 10_00}
+      assert metadata.spec == "@type(t :: %__MODULE__{code: String.t(), price: integer})"
+    end
+  end
 end
